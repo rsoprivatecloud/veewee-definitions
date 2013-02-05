21ac36170f068201b24ee144cb341495949f430d
JCallicoat <jordan.callicoat@rackspace.com>
2013-02-04 18:59:56 -0600

    Add centos 6.3 cloud-init definition

---
+++ centos-6.3-cloud-init/postinstall.sh
@@ -0,0 +1,26 @@
+
+#cat > /etc/yum.repos.d/epel.repo <<END
+#[epel]
+#name=epel
+#baseurl=http://mirror.us.leaseweb.net/epel/5/x86_64/
+#gpgcheck=0
+#END
+
+yum -y update
+yum -y upgrade
+
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+rpm -Uvh epel-release*
+
+yum install -y cloud-init
+
+#/usr/sbin/useradd -m stack
+#echo 'stack ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+sed -i 's/^user: ec2-user/user: stack/g' /etc/cloud/cloud.cfg
+
+cat > /etc/rc.local <<END
+echo -n > /etc/udev/rules.d/70-persistent-net.rules
+exit
+END
+
+exit
