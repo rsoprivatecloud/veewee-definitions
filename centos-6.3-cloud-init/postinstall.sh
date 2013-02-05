fdeeed20b8a682f3a6fda06d759ff8d03f3fb4d5
JCallicoat <jordan.callicoat@rackspace.com>
2013-02-04 19:16:35 -0600

    Use EPEL 6 in centos 6.3 cloud-init definition

--- centos-6.3-cloud-init/postinstall.sh
+++ centos-6.3-cloud-init/postinstall.sh
@@ -9,7 +9,7 @@
 yum -y update
 yum -y upgrade
 
-wget http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh epel-release*
 
 yum install -y cloud-init
