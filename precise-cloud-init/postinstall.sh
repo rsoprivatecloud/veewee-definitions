89dd9fb09d1a28f0fb477b440ab6bebfaa90dd9e
JCallicoat <jordan.callicoat@rackspace.com>
2013-02-28 21:49:48 -0600

    pin partition order on cent with %pre

--- precise-cloud-init/postinstall.sh
+++ precise-cloud-init/postinstall.sh
@@ -5,15 +5,9 @@ apt-get update
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 
-apt-get install -y vim-tiny wget ssl-cert curl acpid cloud-init cloud-utils #cloud-initramfs-growroot
+apt-get install -y vim-tiny wget ssl-cert curl acpid cloud-init cloud-utils cloud-initramfs-growroot
 
 sed -i 's/^user: ubuntu/user: stack/g' /etc/cloud/cloud.cfg
-
-cat > /etc/rc.local <<END
-    [[ ! -f /etc/dont_grow ]] && \
-        growpart /dev/vda 2 | fgrep 'CHANGED:' && \
-        shutdown -r now
-END
 
 # some os stuffs
 #usermod -a -G sudo $SUDO_USER
