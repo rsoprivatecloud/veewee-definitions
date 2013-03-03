e48ffee8d8f0000392c8d419a6acb41008bf0e8d
JCallicoat <jordan.callicoat@rackspace.com>
2013-03-03 06:39:30 -0600

    make precise use cloud-initramfs-growroot again

--- precise-cloud-init/postinstall.sh
+++ precise-cloud-init/postinstall.sh
@@ -5,18 +5,18 @@ apt-get update
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 
-apt-get install -y vim-tiny wget ssl-cert curl acpid cloud-init cloud-utils # cloud-initramfs-growroot
+apt-get install -y vim-tiny wget ssl-cert curl acpid cloud-init cloud-utils cloud-initramfs-growroot
 
 sed -i 's/^user: ubuntu/user: stack/g' /etc/cloud/cloud.cfg
 
-cat > /etc/rc.local <<END
-    PATH="$PATH:/sbin"
-    #ROOTPART=\`parted -s /dev/vda print | awk '/boot/ {print \$1}'\`
-    ROOTPART=\`sfdisk -d /dev/vda 2>&1 | awk '\$1 ~ "/dev/vda" && \$8 == "bootable" {gsub("/dev/vda", "", \$1); print \$1}'\`
-    [[ ! -f /etc/dont_grow ]] && \\
-        growpart /dev/vda \$ROOTPART | fgrep 'CHANGED:' && \\
-        shutdown -r now
-END
+# cat > /etc/rc.local <<END
+#     PATH="$PATH:/sbin"
+#     #ROOTPART=\`parted -s /dev/vda print | awk '/boot/ {print \$1}'\`
+#     ROOTPART=\`sfdisk -d /dev/vda 2>&1 | awk '\$1 ~ "/dev/vda" && \$8 == "bootable" {gsub("/dev/vda", "", \$1); print \$1}'\`
+#     [[ ! -f /etc/dont_grow ]] && \\
+#         growpart /dev/vda \$ROOTPART | fgrep 'CHANGED:' && \\
+#         shutdown -r now
+# END
 
 echo "stack        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
