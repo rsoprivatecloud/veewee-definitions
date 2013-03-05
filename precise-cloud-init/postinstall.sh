11fcd5e5742e25cfed89a01aa83d9edd414e4081
JCallicoat <jordan.callicoat@rackspace.com>
2013-03-04 22:49:11 -0600

    fix cloud-init sources

--- precise-cloud-init/postinstall.sh
+++ precise-cloud-init/postinstall.sh
@@ -5,13 +5,14 @@ apt-get update
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 
-echo "cloud-init cloud-init/datasources string NoCloud, OVF, Ec2" > /tmp/debconf-selections
-/usr/bin/debconf-set-selections /tmp/debconf-selections
-rm -f /tmp/debconf-selections
+#echo "cloud-init cloud-init/datasources string NoCloud, OVF, Ec2" > /tmp/debconf-selections
+#/usr/bin/debconf-set-selections /tmp/debconf-selections
+#rm -f /tmp/debconf-selections
 
 apt-get install -y vim-tiny wget ssl-cert curl acpid cloud-init cloud-utils cloud-initramfs-growroot
 
 sed -i 's/^user: ubuntu/user: stack/g' /etc/cloud/cloud.cfg
+sed -i 's/^# datasource_/datasource_/' /etc/cloud/cloud.cfg
 
 echo "stack        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
