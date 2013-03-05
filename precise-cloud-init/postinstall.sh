0b363cde4c63e336eba5a5390f4325a00f2241d9
JCallicoat <jordan.callicoat@rackspace.com>
2013-03-04 22:32:08 -0600

    precise finaler

--- precise-cloud-init/postinstall.sh
+++ precise-cloud-init/postinstall.sh
@@ -5,7 +5,10 @@ apt-get update
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" upgrade
 yes '' | apt-get -y -o Dpkg::Options::="--force-confnew" dist-upgrade
 
-echo "cloud-init cloud-init/datasources string NoCloud, OVF, Ec2" | /usr/bin/debconf-set-selections
+echo "cloud-init cloud-init/datasources string NoCloud, OVF, Ec2" > /tmp/debconf-selections
+/usr/bin/debconf-set-selections /tmp/debconf-selections
+rm -f /tmp/debconf-selections
+
 apt-get install -y vim-tiny wget ssl-cert curl acpid cloud-init cloud-utils cloud-initramfs-growroot
 
 sed -i 's/^user: ubuntu/user: stack/g' /etc/cloud/cloud.cfg
@@ -14,4 +17,5 @@ echo "stack        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
 
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet console=tty console=ttyS0 console=hvc0"/' /etc/default/grub
+install-grub /dev/vda
 update-grub
