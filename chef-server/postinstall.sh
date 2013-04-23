6eca7c511f5537ab9e8acdfe8c6ed0a725cbdc38
JCallicoat <jordan.callicoat@rackspace.com>
2013-04-23 17:54:27 -0500

    rename chef-server def

--- chef-server/postinstall.sh
+++ chef-server/postinstall.sh
@@ -12,7 +12,7 @@ CHEF_SERVER_VERSION=${CHEF_SERVER_VERSION:-11.0.6}
 PRIMARY_INTERFACE=$(ip route list match 0.0.0.0 | awk 'NR==1 {print $5}')
 #MY_IP=$(ip addr show dev ${PRIMARY_INTERFACE} | awk 'NR==3 {print $2}' | cut -d '/' -f1)
 MY_IP=169.254.123.2
-CHEF_UNIX_USER=${CHEF_UNIX_USER:-root}
+CHEF_UNIX_USER=${CHEF_UNIX_USER:-rack}
 # due to http://tickets.opscode.com/browse/CHEF-3849 CHEF_FE_PORT is not used yet
 CHEF_FE_PORT=${CHEF_FE_PORT:-80}
 CHEF_FE_SSL_PORT=${CHEF_FE_SSL_PORT:-443}
@@ -42,7 +42,8 @@ ${HOMEDIR}/.chef/chef-validator.pem
 ch3fz11
 EOF
     # setup the path
-    echo 'export PATH=${PATH}:/opt/chef-server/embedded/bin' >> ${HOMEDIR}/.profile
+    #echo 'export PATH=${PATH}:/opt/chef-server/embedded/bin' >> ${HOMEDIR}/.profile
+    sed 's|\(PATH="[^"]*\)|&:/opt/chef-server/embedded/bin|' /etc/environment
 fi
 
 apt-get autoremove
